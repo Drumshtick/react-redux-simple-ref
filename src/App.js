0abbfff898ff551daa4daa6135c475def0bcab49
Drumshtick <ekodnar@outlook.com>
2022-02-21 19:32:15 -0600

    basic implementation of devTools for redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { connect } from "react-redux";
+import { startAction } from "./actions/startAction";
+import { stopAction } from "./actions/stopAction";
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img 
+          src={logo} 
+          className={
+            "App-logo" + 
+            (props.rotating ? "":" App-logo-paused")
+          } 
+          alt="logo" 
+          onClick={
+            props.rotating ? 
+              props.stopAction : props.startAction
+          }
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +35,13 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux"; // makes the Redux store available to any nested components that have been wrapped in the connect function
+
+import configureStore from "./store/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={configureStore()}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
